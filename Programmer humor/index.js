563d68dff1b3e54df0181ca62951e8ddbcbb224f
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-05-04 13:22:15 +0530

    Eightcommit

--- Programmer humor/index.js
+++ Programmer humor/index.js
@@ -1,21 +1,50 @@
-const img = document.getElementById('img');
-const postReg = new XMLHttpRequest();
-postReg.open('get',"https://xkcd.now.sh/?comic=614")
-postReg.send();
-postReg.addEventListener('load', function(){
-    console.log(JSON.parse(postReg.response))
-    const link = JSON.parse(postReg.response).img;
-    img.src="link";
-    document.body.appendChild(img);
-});
-axios
-.get("https://xkcd.now.sh/?comic=614")
-.then(function(res){
-console.log(res.data.img)
-const imgSrc = res.data.img;
-img.src =imgSrc
-})
-.catch(err =>{
-    console.log("there is an error",err)
-})
-.finally
+function reqListener() {
+         const data = JSON.parse(this.responseText);
+         let status = data.status;
+         if(status >= 400){
+             console.log('erro!')
+         }else{
+             console.log('success!')
+         }
+         console.log(data);
+
+         let img =document.createElement('img')
+         document.body.appendChild(img)
+         img.src = data.img
+     }
+     var oReq = new XMLHttpRequest();
+     let status = oReq.status;
+
+     oReq.addEventListener("load", reqListener);
+     oReq.open('GET', 'https://xkcd.now.sh/?comic=614');
+     oReq.send();
+
+
+
+
+
+    
+    let data
+    let img =document.createElement('img')
+    document.body.appendChild(img)
+
+    axios.get('https://xkcd.now.sh/?comic=614')
+
+
+
+    .then(function (response) {
+        console.log(response);
+        
+        data=response.data.img
+        console.log(data)
+        img.src = data
+
+    })
+    .catch(function (error) {
+        console.log('error');
+    })
+    .finally(function () {
+
+    });  
+
+

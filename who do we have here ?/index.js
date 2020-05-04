c3be49743413ebcfa642fcba75956fce0332a375
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-05-04 13:14:27 +0530

    Seventeencommit

--- who do we have here ?/index.js
+++ who do we have here ?/index.js
@@ -1,16 +1,30 @@
-const postReg = new XMLHttpRequest();
-postReg.open('get', "https://www.randomuser.me/api")
-postReg.send();
-const btn = document.getElementById("btn")
-function get() {
-    const data = JSON.parse(postReg.response)
-    console.log(data)
+function reqListener() {
+        const facts = JSON.parse(this.responseText);
+         console.log(facts);
+     }
+    var xhr  = new XMLHttpRequest();
+     console.log("initializing XMLHttpRequest", xhr);
+     xhr.addEventListener("load", reqListener);
+     xhr.open('GET', 'https://www.randomuser.me/api');
+     console.log("making a request...")
 
-};
-btn.addEventListener("click", get);
+     xhr.send();
+     console.log("sent the request, let's see...");
+
+     console.log(axios);
+
+     axios.get('https://www.randomuser.me/api')
 
 
-axios.get("https://www.randomuser.me/api").then(function (event) {
-    console.log(event.data)
+     .then(function (response) {
+         console.log('GetV', response);
+     })
+     .catch(function (error) {
+         console.log('an error???????????', error);
+     })
+     .finally(function () {
+         
+         console.log('Hey!'
+         )
+ });  
 
-}); 
